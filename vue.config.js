153566d4f40a3d52d0a1d56679337640ec76836b
metheany <metheany.thorn@gmail..com>
2020-03-17 08:52:40 +0700

    update prerender

--- vue.config.js
+++ vue.config.js
@@ -6,32 +6,10 @@ module.exports = {
   },
   pluginOptions: {
     prerenderSpa: {
-      renderRoutes: [
-        "/",
-        "/about",
-        "/contact",
-        "/service/ai",
-        "/service/pwa",
-        "/service/blockchain",
-        "/service/iot",
-        "/service/arvr",
-        "/service/marketing",
-        "/service/cc",
-        "/service/odoo",
-        "/service/rpa",
-        "/service/cybersecurity",
-        "/service/security",
-        "/solution/security",
-        "/solution/hrm",
-        "/solution/mapping",
-        "/solution/kpi",
-        "/solution/ls",
-        "/solution/contactcenter",
-        "/solution/qrcode",
-        "/solution/sms"
-      ],
+      renderRoutes: ["/"],
       useRenderEvent: true,
-      onlyProduction: true
+      onlyProduction: true,
+      headless: true
     },
     i18n: {
       locale: "ja",
