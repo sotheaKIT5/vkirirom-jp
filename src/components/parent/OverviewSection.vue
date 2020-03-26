a87aeca965cf72a3dde5d54af0a7173aa48cccd5
metheany <metheany.thorn@gmail..com>
2020-03-26 08:53:37 +0700

    update OverviewSection

--- src/components/parent/OverviewSection.vue
+++ src/components/parent/OverviewSection.vue
@@ -7,42 +7,63 @@
       ]"
     >
       <v-col cols="12" sm="6">
-        <h3 style="margin-bottom: 30px">A2A Town (Cambodia) Co., LTd.</h3>
+        <h2 class="heading mb-3">vKirirom Japan 株式会社</h2>
         <table>
           <tr>
-            <th>設立</th>
-            <td>2011年12月</td>
-          </tr>
-          <tr>
             <th>資本金</th>
-            <td>13 millionUSD</td>
+            <td>10百万円</td>
           </tr>
           <tr>
-            <th>CEO</th>
-            <td>猪塚 武</td>
+            <th>議決権</th>
+            <td>vKirirom Pte. Ltd. 100%</td>
           </tr>
           <tr>
-            <th>親会社</th>
-            <td>vKirirom Pte.Ltd.</td>
+            <th>代表取締役社長</th>
+            <td>猪塚武</td>
+          </tr>
+          <tr>
+            <th>取締役</th>
+            <td>有澤和歌子</td>
+          </tr>
+          <tr>
+            <th>取締役</th>
+            <td>黒瀬千尋</td>
+          </tr>
+          <tr>
+            <th>監査役</th>
+            <td>別宮健三郎</td>
           </tr>
           <tr>
             <th>事業内容</th>
-            <td>
-              1. リゾート経営Vkirirom Pine Resort <br />
-              2.大学経営Kirirom Institute of Technology <br />
-              3. 不動産サービス <br />
-              4. IT開発サービス
-            </td>
+            <td>IT事業、マーケティング支援事業</td>
+          </tr>
+          <tr>
+            <th><h2 class="heading mt-5 mb-3">沿革</h2></th>
+          </tr>
+          <tr>
+            <th>2011</th>
+            <td>A2A Town (Cambodia) Co., Ltd 設立</td>
+          </tr>
+          <tr>
+            <th>2014</th>
+            <td>vKirirom Pte. Ltd.設立</td>
+          </tr>
+          <tr>
+            <th>2014</th>
+            <td>キリロム工科大学設立</td>
+          </tr>
+          <tr>
+            <th>2018</th>
+            <td>vKirirom Japan株式会社設立</td>
+          </tr>
+          <tr>
+            <th>2018</th>
+            <td>vKirirom International School(小学校) 設立</td>
           </tr>
         </table>
       </v-col>
       <v-col cols="12" sm="6">
-        <v-img
-          height="100%"
-          max-height="355px"
-          src="@/assets/images/city.jpg"
-          alt="city view"
-        />
+        <v-img height="100%" src="@/assets/images/city.jpg" alt="city view" />
       </v-col>
     </v-row>
   </v-container>
@@ -69,13 +90,12 @@ table > tr
     /*border-bottom: 1px solid #eee*/
     margin-right: 15px
 th, td
-    padding: 5px 0
+    padding: 2px 0
     text-align: left
     line-height: 30px
 table > tr > th
     font-weight: bold
     font-size: 14px
-    padding-right: 30px
     vertical-align: top
 table > tr > td
     font-weight: normal
