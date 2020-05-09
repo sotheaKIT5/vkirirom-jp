364dea45fddb62b0d9e40bd55549720d5e81b6c5
metheany <metheany.thorn@gmail..com>
2020-05-09 12:01:02 +0700

    render news page

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,7 @@ module.exports = {
         "/contact",
         "/about",
         "/startup",
+        "/news",
         "/cloud-crew",
         "/engineer-recruitment",
         "/offshore-development"
