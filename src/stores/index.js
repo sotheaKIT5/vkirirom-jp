0c4f16ef64a703ca513bca398380511d3eb23769
metheany <metheany.thorn@gmail..com>
2020-07-29 11:10:41 +0700

    remove useless files

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,18 +1,1 @@
-import Vue from "vue";
-import Vuex from "vuex";
-
 // store
-import { ServiceStore } from "./modules/service-store";
-import { ServiceDetailStore } from "./modules/service-detail-store";
-
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  modules: {
-    serviceContent: ServiceStore,
-    serviceDetailContent: ServiceDetailStore
-  },
-  state: {},
-  mutations: {},
-  actions: {}
-});
